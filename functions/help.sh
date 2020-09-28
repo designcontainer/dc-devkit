b6950ca3492b54ce37d34170ab7e5958b9c4927b
RostiMelk <hello@rosti.no>
2020-09-28 21:26:01 +0200

    small text changes

--- functions/help.sh
+++ functions/help.sh
@@ -11,14 +11,14 @@ Basic Commands:
     ${cmd}new${end}                                 Clone a new site from WP Engine
 
 Development:
-    ${cmd}restart${end}                             Restarts MAMP
-    ${cmd}start${end}                               Starts MAMP
-    ${cmd}stop${end}                                Stops MAMP
-    ${cmd}hosts${end}                               Opens hosts file
-    ${cmd}vhosts${end}                              Opens virtual hosts file
+    ${cmd}restart${end}                             Restarts MAMP - not working
+    ${cmd}start${end}                               Starts MAMP - not working
+    ${cmd}stop${end}                                Stops MAMP - not working
+    ${cmd}hosts${end}                               Opens hosts file - not working
+    ${cmd}vhosts${end}                              Opens virtual hosts file - not working
     
 Update:
-    ${cmd}update${end}                              Updates site cloner to latest version
+    ${cmd}update${end}                              Updates site cloner to latest version - not working
 
 Configuration:
     ${cmd}setup${end}                               Re-runs the setup wizard
