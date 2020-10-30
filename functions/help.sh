2d31e82a83ae9aeaf0c2406a49eeed69f5ce415f
RostiMelk <hello@rosti.no>
2020-10-30 13:01:31 +0100

    fixed typo in help

--- functions/help.sh
+++ functions/help.sh
@@ -9,10 +9,10 @@ Usage:
     ${prefix} [command]
 
 Basic Commands:
-    ${cmd}-o, open${end}                Opens site in browser from folder
+    ${cmd}-o, open${end}                            Opens site in browser from folder
     ${cmd}new <site name>${end}                     Create a blank Wordpress install with DC skeleton theme
     ${cmd}clone <wpe install name>${end}            Clone a new site from WP Engine
-    ${cmd}database${end}                            Fetch a new database form server for an existing local install
+    ${cmd}database${end}                            Fetch a new database from server for an existing local install
     ${cmd}database local${end}                      Export the current local database
     ${cmd}wpe <page>${end}                          Open WP Engine panel for site, <help, -h> to see args
 
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # Meta
-version="0.14.0"
+version="0.14.1"
 prefix="dev"
 
 # vars
