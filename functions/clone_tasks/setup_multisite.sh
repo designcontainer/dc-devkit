f5dc102d35135a7ef05314d655e43147da0214f9
RostiMelk <hello@rosti.no>
2021-01-20 12:55:42 +0100

    cleanup

--- functions/clone_tasks/setup_multisite.sh
+++ functions/clone_tasks/setup_multisite.sh
@@ -70,21 +70,21 @@ setup_multisite() {
         fi
 
         # Add vhost
-        echo '<VirtualHost *:80>' >> $vhosts_conf
-        echo 'ServerName '$new_domain >> $vhosts_conf
-        echo 'DocumentRoot "'$PWD'"' >> $vhosts_conf
-        echo '    <Directory "'$PWD'">' >> $vhosts_conf
-        echo '        Options FollowSymLinks' >> $vhosts_conf
-        echo '        AllowOverride None' >> $vhosts_conf
-        echo '    </Directory>' >> $vhosts_conf
-        echo '    <Directory "'$PWD'">' >> $vhosts_conf
-        echo '        Options Indexes FollowSymLinks MultiViews' >> $vhosts_conf
-        echo '        AllowOverride All' >> $vhosts_conf
-        echo '        Order allow,deny' >> $vhosts_conf
-        echo '        allow from all' >> $vhosts_conf
-        echo '    </Directory>' >> $vhosts_conf
-        echo '</VirtualHost>' >> $vhosts_conf
-        echo '' >> $vhosts_conf
+        echo '<VirtualHost *:80>'                                   >> $vhosts_conf
+        echo 'ServerName '$new_domain                               >> $vhosts_conf
+        echo 'DocumentRoot "'$PWD'"'                                >> $vhosts_conf
+        echo '    <Directory "'$PWD'">'                             >> $vhosts_conf
+        echo '        Options FollowSymLinks'                       >> $vhosts_conf
+        echo '        AllowOverride None'                           >> $vhosts_conf
+        echo '    </Directory>'                                     >> $vhosts_conf
+        echo '    <Directory "'$PWD'">'                             >> $vhosts_conf
+        echo '        Options Indexes FollowSymLinks MultiViews'    >> $vhosts_conf
+        echo '        AllowOverride All'                            >> $vhosts_conf
+        echo '        Order allow,deny'                             >> $vhosts_conf
+        echo '        allow from all'                               >> $vhosts_conf
+        echo '    </Directory>'                                     >> $vhosts_conf
+        echo '</VirtualHost>'                                       >> $vhosts_conf
+        echo ''                                                     >> $vhosts_conf
 
         # Add desired domain to hosts
         # Message for password promt, will show on first domain only
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # Meta
-version="0.17.0"
+version="0.17.1"
 prefix="dev"
 
 # vars
