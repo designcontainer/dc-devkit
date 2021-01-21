7f1a124005503e9a2e7919525699d65c7c7ba228
RostiMelk <hello@rosti.no>
2021-01-21 16:07:01 +0100

    flags cleanup

--- functions/help.sh
+++ functions/help.sh
@@ -10,17 +10,17 @@ Usage:
 
 Basic commands:
     ${cmd}-o, open${end}                            Opens site in browser from folder
-    ${cmd}-w, wpe <page>${end}                      Open WP Engine panel for site, <-h, --help> to see args
+    ${cmd}-w, wpe <page>${end}                      Open WP Engine panel for site. (-h, --help forargs)
     ${cmd}-t, test${end}                            Test if site is devkit ready
 
 Development:
     ${cmd}new <site name>${end}                     Create a blank Wordpress install with DC skeleton theme
     ${cmd}clone <wpe install name>${end}            Clone a new site from WP Engine
-    ${cmd}db, database <arg>${end}                  Fetch or export databases, <-h, --help> to see args
+    ${cmd}db, database <arg>${end}                  Fetch or export databases. (-h, --help forargs)
     ${cmd}remove${end}                              Remove site from computer. Includes files, vhosts and database
 
 Server:
-    ${cmd}mamp${end}                                Restarts MAMP, <-h, --help> to see args
+    ${cmd}mamp${end}                                Restarts MAMP. (-h, --help forargs)
     ${cmd}hosts${end}                               Opens hosts file
     ${cmd}vhosts${end}                              Opens virtual hosts file
     ${cmd}phpmyadmin${end}                          Opens local phpmyadmin
