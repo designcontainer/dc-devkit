3218c16c6e4ce99308f8943206fe7a7798139cd3
gbratendc <cecce@designcontainer.no>
2022-02-02 15:37:27 +0100

    Optional github accounts
    - If user adds "Github SHH Config key" then it will use that git url instead of default

--- functions/clone_tasks/set_git_url.sh
+++ functions/clone_tasks/set_git_url.sh
@@ -1,12 +1,20 @@
 set_git_url() {
-    wpegiturl="git@git.wpengine.com:production/$installname.git"
-    ghgiturl="git@github.com:designcontainer/$installname.git"
-    git-remote-url-reachable() {
-        git ls-remote "$1" CHECK_GIT_REMOTE_URL_REACHABILITY >/dev/null 2>&1
-    }
-    if git-remote-url-reachable $ghgiturl ; then
-        giturl=$ghgiturl
-    else
-        giturl=$wpegiturl
-    fi
+	wpegiturl="git@git.wpengine.com:production/$installname.git"
+	ghgiturl="git@github.com:designcontainer/$installname.git"
+
+	# Use custom githubSHH if exists
+	if $gitSSHhost ; then
+		ghgiturl="$gitSSHId:designcontainer/$installname"
+	else
+		ghgiturl="git@github.com:designcontainer/$installname.git"
+	fi
+
+	git-remote-url-reachable() {
+		git ls-remote "$1" CHECK_GIT_REMOTE_URL_REACHABILITY >/dev/null 2>&1
+	}
+	if git-remote-url-reachable $ghgiturl ; then
+		giturl=$ghgiturl
+	else
+		giturl=$wpegiturl
+	fi
 }
